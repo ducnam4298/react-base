c9185cf0f006f9c3ecd048214d678e7bd07de2f8
ducnam4298 <ducnam4298@gmail.com>
2022-03-09 16:00:22 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from 'layouts';
-import reportWebVitals from './reportWebVitals';
 import { I18nextProvider } from 'react-i18next';
 import Providers from 'providers';
 import i18next from 'i18next';
@@ -18,8 +17,3 @@ ReactDOM.render(
   </I18nextProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
