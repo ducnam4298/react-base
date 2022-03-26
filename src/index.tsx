27ef4b312ce1785fa1f041f5c76bb76c85dbec11
ducnam4298 <ducnam4298@gmail.com>
2022-03-27 02:13:52 +0700

    update kit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import { App } from 'layouts';
+// import { I18nextProvider } from 'react-i18next';
+// import Providers from 'providers';
+// import i18next from 'i18next';
+// import './i18next';
+
+// ReactDOM.render(
+//   <I18nextProvider i18n={i18next}>
+//     <React.StrictMode>
+//       <Providers>
+//         <App />
+//       </Providers>
+//     </React.StrictMode>
+//   </I18nextProvider>,
+//   document.getElementById('root')
+// );
+
+// scroll bar
+import 'simplebar/src/simplebar.css';
+
 import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from 'layouts';
-import { I18nextProvider } from 'react-i18next';
-import Providers from 'providers';
-import i18next from 'i18next';
-import './i18next';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+//
+import { App } from './layouts';
+import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
+
+// ----------------------------------------------------------------------
 
 ReactDOM.render(
-  <I18nextProvider i18n={i18next}>
-    <React.StrictMode>
-      <Providers>
-        <App />
-      </Providers>
-    </React.StrictMode>
-  </I18nextProvider>,
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>,
   document.getElementById('root')
 );
+
+// If you want to enable client cache, register instead.
+serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/layouts/app.tsx
+++ src/layouts/app.tsx
@@ -1,40 +1,64 @@
-import React, { useContext } from 'react';
-import { Theme } from 'themes';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import styled from '@emotion/styled';
+// import React, { useContext } from 'react';
+// import { Theme } from 'themes';
+// import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+// import styled from '@emotion/styled';
+
+// const App = () => {
+//   const { isLight } = useContext(Theme);
+//   return (
+//     <Router>
+//       <Body style={{ background: isLight ? '' : '' }}>
+//         <Routes>
+//           <Route path="/signin" element={<h1>signin</h1>} />
+//           <Route path="/signup" element={<h1>signup</h1>} />
+//           <Route path="/forgot" element={<h1>forgot</h1>} />
+//           <Route path="/" element={<h1>home</h1>} />
+//           <Route path="/bill" element={<h1>bill</h1>} />
+//           <Route path="/brand" element={<h1>brand</h1>} />
+//           <Route path="/delivery" element={<h1>delivery</h1>} />
+//           <Route path="/order" element={<h1>order</h1>} />
+//           <Route path="/product" element={<h1>product</h1>} />
+//           <Route path="/role" element={<h1>role</h1>} />
+//           <Route path="/setting" element={<h1>setting</h1>} />
+//           <Route path="/user" element={<h1>user</h1>} />
+//           <Route path="/customer" element={<h1>customer</h1>} />
+//         </Routes>
+//       </Body>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+// const Body = styled.div`
+//   background-color: #1a1d23;
+//   width: 100%;
+//   height: 100%;
+//   min-height: 100vh;
+//   display: flex;
+//   flex-direction: column;
+// `;
+
+// routes
+import Router from '../routes';
+// theme
+import ThemeConfig from '../theme';
+import GlobalStyles from '../theme/globalStyles';
+// components
+import ScrollToTop from '../components/ScrollToTop';
+import { BaseOptionChartStyle } from '../components/charts/BaseOptionChart';
+
+// ----------------------------------------------------------------------
 
 const App = () => {
-  const { isLight } = useContext(Theme);
   return (
-    <Router>
-      <Body style={{ background: isLight ? '' : '' }}>
-        <Routes>
-          <Route path="/signin" element={<h1>signin</h1>} />
-          <Route path="/signup" element={<h1>signup</h1>} />
-          <Route path="/forgot" element={<h1>forgot</h1>} />
-          <Route path="/" element={<h1>home</h1>} />
-          <Route path="/bill" element={<h1>bill</h1>} />
-          <Route path="/brand" element={<h1>brand</h1>} />
-          <Route path="/delivery" element={<h1>delivery</h1>} />
-          <Route path="/order" element={<h1>order</h1>} />
-          <Route path="/product" element={<h1>product</h1>} />
-          <Route path="/role" element={<h1>role</h1>} />
-          <Route path="/setting" element={<h1>setting</h1>} />
-          <Route path="/user" element={<h1>user</h1>} />
-          <Route path="/customer" element={<h1>customer</h1>} />
-        </Routes>
-      </Body>
-    </Router>
+    <ThemeConfig>
+      <ScrollToTop />
+      <GlobalStyles />
+      <BaseOptionChartStyle />
+      <Router />
+    </ThemeConfig>
   );
 };
 
 export default App;
-
-const Body = styled.div`
-  background-color: #1a1d23;
-  width: 100%;
-  height: 100%;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
