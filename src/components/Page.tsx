30dbdac587954b1e227804aea6a98a4aa0aace40
ducnam4298 <ducnam4298@gmail.com>
2022-04-01 15:47:59 +0700

    complete js to ts

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,15 +5,18 @@ import { Box } from '@mui/material';
 interface UIProps {
   children: React.ReactNode;
   title?: string;
+  className?: string;
 }
 
-const Page = forwardRef((props: UIProps, ref) => (
-  <Box ref={ref}>
-    <Helmet>
-      <title>{props.title}</title>
-    </Helmet>
-    {props.children}
-  </Box>
-));
+const Page = forwardRef((props: UIProps, ref) => {
+  return (
+    <Box ref={ref} className={props.className}>
+      <Helmet>
+        <title>{props.title}</title>
+      </Helmet>
+      {props.children}
+    </Box>
+  );
+});
 
 export default Page;
