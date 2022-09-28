dd179cf244360fb8b3340840cb88a438b0bfcd58
ducnam4298 <ducnam4298@gmail.com>
2022-09-28 17:48:29 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 // import React from 'react';
-// import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import { App } from 'layouts';
 // import { I18nextProvider } from 'react-i18next';
 // import Providers from 'providers';
 // import i18next from 'i18next';
 // import './i18next';
-
-// ReactDOM.render(
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
 //   <I18nextProvider i18n={i18next}>
 //     <React.StrictMode>
 //       <Providers>
