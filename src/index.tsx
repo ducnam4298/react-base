aa2ea5a83e8be2b04f5d18bf152cc4bbf5092d47
ducnam4298 <ducnam4298@gmail.com>
2022-09-28 17:34:54 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -19,19 +19,22 @@
 // );
 
 import 'simplebar/src/simplebar.css';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { App } from './layouts';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
   <HelmetProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </HelmetProvider>,
-  document.getElementById('root')
+  </HelmetProvider>
 );
 
 // If you want to enable client cache, register instead.
