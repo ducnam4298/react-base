ed52c16f4248f35c1bccbc32025795f2d9afcf7b
ducnam4298 <ducnam4298@gmail.com>
2022-12-30 15:05:12 +0700

    update struct folder components

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+import ButtonMui from './ButtonMui';
+import PaginationMui from './PaginationMui';
+import ModalMui from './ModalMui';
+import DrawerMui from './DrawerMui';
+import PopoverMui from './PopoverMui';
+
+export {
+  ButtonMui,
+  PaginationMui,
+  ModalMui,
+  DrawerMui,
+  PopoverMui,
+};
